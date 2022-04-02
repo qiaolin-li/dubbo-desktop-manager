a97a8976d02262b1107a0cc54702b31667c5e95e
qiaolin <liqiaolin@xsyxsc.com>
2022-04-02 18:45:45 +0800

    1、使用主进程渲染进程通讯的方式调用，不再直接调用js访问数据库 2、增加统计人数 3、删除无用的图标

---
+++ src/core/communication/test.js
@@ -0,0 +1,19 @@
+// import communication from './index.js'
+// import cusumer from './cusumer.js'
+
+
+// let data = {
+//     name: "哈哈哈哈",
+//     aaa(ssss) {
+//         console.log(1111);
+//         return "哈哈哈哈"
+//     }
+// }
+// communication.registry(data);
+
+// let proxy = cusumer.wrapper(data);
+
+
+// let ddd = proxy.aaa("哈哈哈哈哈");
+
+// console.log(ddd);
